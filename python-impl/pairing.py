fa3487013c6ea92b87eb2a3c53ad5758c70e248c
Nathan Marley <nathan.marley@gmail.com>
2019-05-11 15:00:59 -0300

    save77

--- python-impl/pairing.py
+++ python-impl/pairing.py
@@ -114,20 +114,3 @@ def ate_pairing_multi(Ps, Qs, ec=default_ec):
     for i in range(len(Qs)):
         prod *= miller_loop(T, Ps[i], Qs[i], ec)
     return final_exponentiation(prod, ec)
-
-
-"""
-Copyright 2018 Chia Network Inc
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
--- python-impl/tests.py
+++ python-impl/tests.py
@@ -25,3 +25,4 @@ def test_vectors():
     assert(sk1.serialize() == bytes.fromhex("022fb42c08c12de3a6af053880199806532e79515f94e83461612101f9412f9e"))
     assert(pk1.get_fingerprint() == 0x26d53247)
     assert(sig1.serialize() == bytes.fromhex("93eb2e1cb5efcfb31f2c08b235e8203a67265bc6a13d9f0ab77727293b74a357ff0459ac210dc851fcb8a60cb7d393a419915cfcf83908ddbeac32039aaa3e8fea82efcb3ba4f740f20c76df5e97109b57370ae32d9b70d256a98942e5806065"))
+    assert(sig1.verify())
