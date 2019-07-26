32690eadfe7a5daf14e6e14cd7204aa032beb40c
Nathan Marley <nathan.marley@gmail.com>
2019-07-26 10:57:24 -0300

    more

--- go-bindings/new/num_test.go
+++ go-bindings/new/num_test.go
@@ -1,6 +1,9 @@
 package num
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestNum(t *testing.T) {
 	num := New()
@@ -14,6 +17,11 @@ func TestNum(t *testing.T) {
 	if num.GetValue() != 5 {
 		t.Error("unexpected value received")
 	}
-
+	value := num.GetValue()
 	num.Free()
+
+	typ := reflect.TypeOf(value)
+	if typ.Name() != "int" {
+		t.Error("got unexpected type")
+	}
 }
